ccd006c299803549bf3ab0ba007ced85068ec667
Alin Cristian <alin.cristian977@gmail.com>
2021-07-06 21:13:56 +0200

    Organised Mover code into Methods

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -9,14 +9,28 @@ public class Mover : MonoBehaviour
 
     private void Start()
     {
-        
+        PrintInstructions();
     }
 
     private void Update()
     {
+        MovePlayer();
+    }
+    
+    private void PrintInstructions() 
+    {
+        Debug.Log("Welcome to the game");
+        Debug.Log("Move your player with WASD or arrow keys");
+        Debug.Log("Don't hit the walls!");
+    }
+
+    private void MovePlayer()
+    {
         float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * _moveSpeed;
         float zValue = Input.GetAxis("Vertical") * Time.deltaTime * _moveSpeed;
-
+        
         transform.Translate(xValue, 0, zValue);
     }
+    
+
 }
