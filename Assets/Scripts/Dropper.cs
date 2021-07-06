196809577799f0f8afc91e799bcefb9a82407463
Alin Cristian <alin.cristian977@gmail.com>
2021-07-06 21:38:01 +0200

    Added sky-falling Obstacles

--- Assets/Scripts/Dropper.cs
+++ Assets/Scripts/Dropper.cs
@@ -1,14 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Dropper : MonoBehaviour
 {
+    [SerializeField] private float _timeToWait = 5f;
+
+    private MeshRenderer _renderer;
+    private Rigidbody _rigidbody;
+
+    private void Awake()
+    {
+        _renderer = GetComponent<MeshRenderer>();
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Start()
+    {
+        _renderer.enabled = false;
+        _rigidbody.useGravity = false;
+    }
+
     private void Update()
     {
-        if(Time.time > 3)
+        if(Time.time > _timeToWait)
         {
-            Debug.Log("3 seconds have elapsed");
+            _renderer.enabled = true;
+            _rigidbody.useGravity = true;
         }
     }
 }
